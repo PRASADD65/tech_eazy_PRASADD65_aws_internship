b26701bbe6cbf87e106230347d936769d53200fc
PRASADD <durgaaprasadd765@gmail.com>
2025-06-12 04:51:08 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -8,8 +8,103 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
   tags = {
     Terraform = "true"
     Environment = "dev"
   }
 }
+
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = module.vpc.vpc_id
+
+  tags = {
+    Name = "my-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = module.vpc.vpc_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_1" {
+  subnet_id      = module.vpc.public_subnets[0]
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_subnet_2" {
+  subnet_id      = module.vpc.public_subnets[1]
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private_subnet_1" {
+  subnet_id      = module.vpc.private_subnets[0]
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private_subnet_2" {
+  subnet_id      = module.vpc.private_subnets[1]
+  route_table_id = aws_route_table.public.id
+}
+
+
+resource "aws_security_group" "web_sg" {
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
