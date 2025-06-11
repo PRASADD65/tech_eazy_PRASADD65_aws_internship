9d18180498cdda1a696ee74aef8debaea02dec0c
PRASADD <durgaaprasadd765@gmail.com>
2025-06-12 04:59:42 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "stage" {
+  description = "Deployment stage (e.g., dev, prod)"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "key_name" {
+  description = "Name of the SSH key pair"
+  type        = string
+}
