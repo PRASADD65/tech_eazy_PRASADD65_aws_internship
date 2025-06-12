d237d2f3d1ab7196acf7b20f5a256f591d888e7a
PRASADD <durgaaprasadd765@gmail.com>
2025-06-12 05:56:39 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,16 +1,23 @@
+resource "aws_instance" "app_instance" {
+  ami           = "ami-0c55b159cbfafe1f0" # Example AMI ID
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  subnet_id     = module.vpc.public_subnets[0]
+  depends_on    = [
+    aws_key_pair.my_key,
+    module.vpc,
+    aws_security_group.web_sg
+  ]
 
-  resource "aws_instance" "app_instance" {
-  ami           = "ami-0c55b159cbfafe1f0" # Example AMI ID
-  instance_type = var.instance_type
-  key_name      = var.key_name
-  subnet_id     = module.vpc.public_subnets[0]
-  depends_on    = security_groups = [aws_security_group.web_sg.name]
+  tags = {
+    Name = "${var.stage}-${var.instance_name}"
+  }
 }
 
 resource "aws_autoscaling_schedule" "shutdown_schedule" {
-  scheduled_action_name = "shutdown-instance"
-  min_size              = 0
-  max_size              = 0
-  desired_capacity      = 0
-  recurrence            = "0 0 * * *" # Example cron expression for daily shutdown
+  scheduled_action_name = "shutdown-instance"
+  min_size              = 0
+  max_size              = 0
+  desired_capacity      = 0
+  recurrence            = "*/15 * * * *" # Cron expression for shutdown every 15 minutes
 }
