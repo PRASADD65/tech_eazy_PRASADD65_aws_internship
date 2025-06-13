1d8c8ab59a0edbc0495bdf4b997c011862d3c752
PRASADD <durgaaprasadd765@gmail.com>
2025-06-13 14:35:02 +0530

    Delete locals.tf

--- locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  config_file_map = {
-    dev  = "dev_config.json"
-    prod = "prod_config.json"
-  }
-
-  config_file = lookup(local.config_file_map, lower(var.stage), "dev_config.json")
-}
-#This maps stage like "Dev" or "Prod" to the correct config file, with "dev" as fallback.
