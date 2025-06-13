17ab1437597ed00a797bddb6759af8a1fa71260d
PRASADD <durgaaprasadd765@gmail.com>
2025-06-13 20:59:47 +0530

    project 2

--- output.tf
+++ output.tf
@@ -2,7 +2,6 @@ output "vpc_id" {
   description = "ID of the VPC"
   value       = module.vpc.vpc_id
 }
-
 output "public_subnet_ids" {
   description = "IDs of the public subnets"
   value       = module.vpc.public_subnets
