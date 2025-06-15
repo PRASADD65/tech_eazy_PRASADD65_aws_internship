672221446e3d8230f8fc1743bf370a59adac9218
PRASADD <durgaaprasadd765@gmail.com>
2025-06-15 10:48:53 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,3 @@
-# ec2.tf
-
 resource "aws_instance" "app_instance" {
   ami                         = "ami-07891c5a242abf4bc" # REMEMBER TO REPLACE THIS WITH YOUR REGION'S UBUNTU AMI ID
   instance_type               = var.instance_type
@@ -11,15 +9,16 @@ resource "aws_instance" "app_instance" {
   # Attach the IAM Instance Profile to this EC2 instance
   iam_instance_profile = aws_iam_instance_profile.app_instance_profile.name
 
-  # Use the templatefile function to render the user data script
-  user_data = templatefile("${path.module}/user_data.sh.tpl", {
-    # All variables interpolated within user_data.sh.tpl must be passed here.
-    repo_url             = var.repo_url             # Passed as 'repo_url'
-    s3_bucket_name       = var.s3_bucket_name       # Passed as 's3_bucket_name'
-    shutdown_time        = var.shutdown_time        # Passed as 'shutdown_time'
-    stage                = var.stage                # Passed as 'stage'
-    automate_sh_content  = file("${path.module}/automate.sh") # Pass content of automate.sh
-    logs_off_sh_content  = file("${path.module}/logs_off.sh")  # Pass content of logs_off.sh
+ user_data = templatefile("${path.module}/user_data.sh.tpl", {
+  repo_url             = var.repo_url
+  s3_bucket_name       = var.s3_bucket_name
+  shutdown_hour        = var.shutdown_hour
+  stage                = var.stage
+  automate_sh_content  = file("${path.module}/automate.sh")
+  logupload_sh_content = file("${path.module}/logupload.sh")
+  permission_sh_content = file("${path.module}/permission.sh")
+  logupload_service_content = file("${path.module}/logupload.service")
+  region               = var.region
   })
 
   depends_on = [
@@ -31,4 +30,17 @@ resource "aws_instance" "app_instance" {
   tags = {
     Name = "${var.stage}-${var.instance_name}"
   }
+}
+
+
+resource "aws_cloudwatch_event_rule" "start_ec2" {
+  name                = "start-ec2"
+  schedule_expression = var.startup_cron
+}
+
+resource "aws_cloudwatch_event_target" "start_target" {
+  rule      = aws_cloudwatch_event_rule.start_ec2.name
+  target_id = "startEC2"
+  arn       = "arn:aws:ec2:${var.region}::instance/${aws_instance.app_instance.id}"
+  role_arn  = aws_iam_role.ec2_start_role.arn
 }
