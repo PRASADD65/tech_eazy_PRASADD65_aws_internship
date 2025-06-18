50803fef2f940ad48c6b64a3ff566c80a58150d9
PRASADD <durgaaprasadd765@gmail.com>
2025-06-18 22:35:26 +0530

    Update lambdafunction.tf

--- lambdafunction.tf
+++ lambdafunction.tf
@@ -1,20 +1,19 @@
 resource "aws_lambda_function" "start_instance" {
-  filename         = "start_instance.zip"
-  function_name    = "${var.stage}-StartEC2Instance" # Prefixed with stage
-  role             = aws_iam_role.lambda_ec2_control_role.arn # Referencing the correct role from iam.tf
+  filename         = "${path.module}/lambda_code/start_instance.zip" 
+  function_name    = "${var.stage}-StartEC2Instance"
+  role             = aws_iam_role.lambda_ec2_control_role.arn
   handler          = "start_instance.lambda_handler"
   runtime          = "python3.9"
-  source_code_hash = filebase64sha256("start_instance.zip")
+  source_code_hash = filebase64sha256("${path.module}/lambda_code/start_instance.zip") 
   timeout          = 70
 
-  # IMPORTANT: Pass the EC2 Instance ID here!
   environment {
     variables = {
-      EC2_INSTANCE_ID = aws_instance.app_server.id # Assuming your EC2 instance resource is named 'app_server'
+      EC2_INSTANCE_ID = aws_instance.app_server.id
     }
   }
 
-  tags = { # Add tags
+  tags = {
     Name  = "${var.stage}-StartEC2Instance"
     Stage = var.stage
   }
@@ -22,22 +21,21 @@ resource "aws_lambda_function" "start_instance" {
 
 
 resource "aws_lambda_function" "stop_instance" {
-  filename         = "stop_instance.zip"
-  function_name    = "${var.stage}-StopEC2Instance" # Prefixed with stage
-  role             = aws_iam_role.lambda_ec2_control_role.arn # Referencing the correct role from iam.tf
+  filename         = "${path.module}/lambda_code/stop_instance.zip"
+  function_name    = "${var.stage}-StopEC2Instance"
+  role             = aws_iam_role.lambda_ec2_control_role.arn
   handler          = "stop_instance.lambda_handler"
   runtime          = "python3.9"
-  source_code_hash = filebase64sha256("stop_instance.zip")
+  source_code_hash = filebase64sha256("${path.module}/lambda_code/stop_instance.zip")
   timeout          = 70
 
-  # IMPORTANT: Pass the EC2 Instance ID here!
   environment {
     variables = {
-      EC2_INSTANCE_ID = aws_instance.app_server.id # Assuming your EC2 instance resource is named 'app_server'
+      EC2_INSTANCE_ID = aws_instance.app_server.id
     }
   }
 
-  tags = { # Add tags
+  tags = {
     Name  = "${var.stage}-StopEC2Instance"
     Stage = var.stage
   }
