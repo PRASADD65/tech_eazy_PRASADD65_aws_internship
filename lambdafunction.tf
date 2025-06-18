8dad6f1451fea9e08297167a1c8117e1a129ec92
PRASADD <durgaaprasadd765@gmail.com>
2025-06-18 23:51:04 +0530

    Update lambdafunction.tf

--- lambdafunction.tf
+++ lambdafunction.tf
@@ -1,10 +1,10 @@
 resource "aws_lambda_function" "start_instance" {
-  filename         = "${path.module}/lambda_code/start_instance.zip" 
+  filename         = "${path.module}/start_instance.zip" # UPDATED: path to project root
   function_name    = "${var.stage}-StartEC2Instance"
   role             = aws_iam_role.lambda_ec2_control_role.arn
   handler          = "start_instance.lambda_handler"
   runtime          = "python3.9"
-  source_code_hash = filebase64sha256("${path.module}/lambda_code/start_instance.zip") 
+  source_code_hash = filebase64sha256("${path.module}/start_instance.zip") # UPDATED: path to project root
   timeout          = 70
 
   environment {
@@ -21,12 +21,12 @@ resource "aws_lambda_function" "start_instance" {
 
 
 resource "aws_lambda_function" "stop_instance" {
-  filename         = "${path.module}/lambda_code/stop_instance.zip"
+  filename         = "${path.module}/stop_instance.zip" # UPDATED: path to project root
   function_name    = "${var.stage}-StopEC2Instance"
   role             = aws_iam_role.lambda_ec2_control_role.arn
   handler          = "stop_instance.lambda_handler"
   runtime          = "python3.9"
-  source_code_hash = filebase64sha256("${path.module}/lambda_code/stop_instance.zip")
+  source_code_hash = filebase64sha256("${path.module}/stop_instance.zip") # UPDATED: path to project root
   timeout          = 70
 
   environment {
